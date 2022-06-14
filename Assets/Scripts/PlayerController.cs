f86598e14c7e09067e36b477cc8509c2de9764f7
ArtemYakubovich <artem.yakubovich@swagmasha.org>
2022-06-14 13:03:25 +0300

    Добавил движение игрока

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _acceliration;
+
+    public float RotationDirection { get; set; }
+    public float Thrust { get; set; }
+    public bool UseWeapon { get; set; }
+
+    private Rigidbody2D _body;
+    private void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(RotationDirection != 0)
+        {
+            _body.angularVelocity = -RotationDirection * _rotationSpeed;
+        }
+
+        if(Thrust > 0)
+        {
+            Vector2 accelerationDelta = transform.up * _acceliration;
+            _body.velocity += accelerationDelta;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private PlayerController _player;
+    void Update()
+    {
+        _player.RotationDirection = Input.GetAxis("Horizontal");
+        _player.Thrust = Input.GetAxis("Vertical");
+
+        _player.UseWeapon = Input.GetButton("Fire1");
+    }
+}
