74871461deb6326bf768c08fe082403d0ee96aa8
ArtemYakubovich <artem.yakubovich@swagmasha.org>
2022-06-15 18:51:19 +0300

    Добавил метеор и создание его в игре

---
+++ Assets/SpaceShooter/Enemies/Meteor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Meteor : MonoBehaviour
+{
+    [SerializeField] private float _minSpeed = 1f;
+    [SerializeField] private float _maxSpeed = 6f;
+    [SerializeField] private float _torque = 3f;
+
+    private Rigidbody2D _body;
+
+    private void Awake()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+
+    public void Launch()
+    {
+        Vector2 randomDirection = Random.insideUnitCircle.normalized;
+        _body.velocity = randomDirection * Random.Range(_minSpeed, _maxSpeed);
+        float randomTorque = Random.Range(-_torque, _torque);
+        _body.AddTorque(randomTorque);
+    }
+}
---
+++ Assets/SpaceShooter/Enemies/MeteorSpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MeteorSpawner : MonoBehaviour
+{
+    [SerializeField] private Meteor[] _meteors;
+    [SerializeField] private int _initialMeteors;
+
+    private Bounds _screenBounds;
+
+    private void Start()
+    {
+        _screenBounds = FindObjectOfType<ScreenBounds>().TresholdBounds;
+        SpawnMeteors(_initialMeteors);
+    }
+
+    private void SpawnMeteors(int count)
+    {
+        for(int i = 0; i < count; i++)
+        {
+            SpawnRandomMeteor();
+        }
+    }
+
+    private void SpawnRandomMeteor()
+    {
+        int randomMeteorIndex = Random.Range(0, _meteors.Length);
+        Meteor meteorPrefab = _meteors[randomMeteorIndex];
+
+        float yPos = Random.value > 0.5f ? _screenBounds.min.y : _screenBounds.max.y;
+        float xPos = Random.Range(_screenBounds.min.x, _screenBounds.max.x);
+        Vector2 randomOutOfScreenPosition = new Vector2(xPos, yPos);
+
+        Meteor meteor = Instantiate(meteorPrefab, randomOutOfScreenPosition, Quaternion.identity, transform);
+        meteor.Launch();
+    }
+}
