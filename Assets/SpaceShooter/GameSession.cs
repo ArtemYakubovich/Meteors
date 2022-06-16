5f554c5938ce27ebff0b832fcd279be9465ba101
ArtemYakubovich <artempaulavich@gmail.com>
2022-06-16 17:50:52 +0300

    Добавил отоброжение жизней и счёта

--- Assets/SpaceShooter/GameSession.cs
+++ Assets/SpaceShooter/GameSession.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class GameSession : MonoBehaviour
 {
@@ -8,13 +9,18 @@ public class GameSession : MonoBehaviour
     public int Life => _life;
     public int Score => _score;
 
+    public event Action<int> OnScoreChaged;
+    public event Action<int> OnLifeChanged;
+
     public void AddScore(int score)
     {
         _score += score;
+        OnScoreChaged?.Invoke(_score);
     }
 
     public void ChangeLife(int delta)
     {
         _life += delta;
+        OnLifeChanged?.Invoke(_life);
     }
 }
---
+++ Assets/SpaceShooter/UI/HudController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HudController : MonoBehaviour
+{
+    [SerializeField] private Text _lifeText;
+    [SerializeField] private Text _scoreText;
+
+    private void Start()
+    {
+        var session = FindObjectOfType<GameSession>();
+
+        session.OnScoreChaged += ScoreChangedHandler;
+        session.OnLifeChanged += LifeChangedHandler;
+
+        LifeChangedHandler(session.Life);
+        ScoreChangedHandler(session.Score);
+    }
+
+    public void ScoreChangedHandler(int score)
+    {
+        _scoreText.text = score.ToString();
+    }
+
+    public void LifeChangedHandler(int life)
+    {
+        _lifeText.text = $"x{life}";
+    }
+}
